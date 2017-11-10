1bf6f2d0e9c3df2a518c5adcbc910e8fdcba4831
Marta <martadabrowka@gmail.com>
2017-11-10 14:51:59 +0000

    created packages and animal class

---
+++ app/src/main/java/com/example/marta/zoo/animals/Animal.java
@@ -0,0 +1,11 @@
+package com.example.marta.zoo.animals;
+
+/**
+ * Created by marta on 10/11/2017.
+ */
+
+public abstract class Animal {
+    private int cashValue;
+    private int weight;
+    private int age;
+}
---
+++ app/src/main/java/com/example/marta/zoo/animals/Hippo.java
@@ -0,0 +1,8 @@
+package com.example.marta.zoo.animals;
+
+/**
+ * Created by marta on 10/11/2017.
+ */
+
+public class Hippo extends Animal {
+}
