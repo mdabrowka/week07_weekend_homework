967ed3bff457a5ea2b73e569718c9999bc175525
Marta <martadabrowka@gmail.com>
2017-11-10 15:28:52 +0000

    added an add enclosure method to zoo

--- app/src/main/java/com/example/marta/zoo/animals/Zoo/Zoo.java
+++ app/src/main/java/com/example/marta/zoo/animals/Zoo/Zoo.java
@@ -12,4 +12,10 @@ public class Zoo {
     private ArrayList<Enclosure> enclosures;
 
 
+
+    public void addEnclosure() {
+        Enclosure newEnclosure = new Enclosure();
+        enclosures.add(newEnclosure);
+    }
+
 }
