42bc9d86101dfa2051a7a8ec0248344ca56f9c04
Marta <martadabrowka@gmail.com>
2017-11-10 16:41:17 +0000

    added methods to enclosure

--- app/src/main/java/com/example/marta/zoo/animals/enclosures/Enclosure.java
+++ app/src/main/java/com/example/marta/zoo/animals/enclosures/Enclosure.java
@@ -1,6 +1,7 @@
 package com.example.marta.zoo.animals.enclosures;
 
 import com.example.marta.zoo.animals.Animal;
+import com.example.marta.zoo.animals.Hippo;
 
 import java.util.ArrayList;
 
@@ -10,6 +11,23 @@ import java.util.ArrayList;
 
 public class Enclosure<T extends Animal> {
     private ArrayList<T> enclosure;
+    private Animal hippo;
+
+
+    public Enclosure() {
+        this.enclosure = new ArrayList<T>();
+        this.hippo = new Hippo(200, "Polly", 3, 4, 26) {
+        };
+    }
+
+
+    public int enclosureSize() {
+        return enclosure.size();
+    }
+
+    public void addToEnclosure() {
+        enclosure.add((T) hippo);
+    }
 
 
 }
