98f95c40b32bbfde4d6d5b1eb6f49a2e0cd4d056
Marta <martadabrowka@gmail.com>
2017-11-12 12:38:42 +0000

    added visitor class

---
+++ app/src/main/java/com/example/marta/zoo/animals/Zoo/Visitor.java
@@ -0,0 +1,28 @@
+package com.example.marta.zoo.animals.Zoo;
+
+/**
+ * Created by marta on 12/11/2017.
+ */
+
+public class Visitor {
+    private String name;
+    private int budget;
+
+    public Visitor(String name, int budget) {
+        this.name = name;
+        this.budget = budget;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getBudget() {
+        return this.budget;
+    }
+
+    public void payFromBudget(int cash) {
+        this.budget -= cash;
+    }
+
+}
